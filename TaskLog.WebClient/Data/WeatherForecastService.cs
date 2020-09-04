45298763f01d0ed9ec7f14869e6684b4bdd0a53b
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2020-09-04 19:17:33 +0200

    Add badge logic; Needs to be reorganized.

--- TaskLog.WebClient/Data/WeatherForecastService.cs
+++ TaskLog.WebClient/Data/WeatherForecastService.cs
@@ -9,10 +9,10 @@ namespace TaskLog.WebClient.Data
     {
         Calendar calendar = DateTimeFormatInfo.CurrentInfo.Calendar;
         private int id = 0;
-        public List<ProjectTask> GetWeekProjectTasks(IEnumerable<ProjectTask> tasks, int calendarWeek)
-        {
-            return tasks.Where(x => calendar.GetWeekOfYear(x.DateTime, CalendarWeekRule.FirstDay, DayOfWeek.Monday).Equals(calendarWeek)).ToList();
-        }
+        //public List<ProjectTask> GetWeekProjectTasks(IEnumerable<ProjectTask> tasks, int calendarWeek)
+        //{
+        //    return tasks.Where(x => calendar.GetWeekOfYear(x.DateTime, CalendarWeekRule.FirstDay, DayOfWeek.Monday).Equals(calendarWeek)).ToList();
+        //}
 
         public int GetCurrentCalendarWeek()
         {
@@ -30,22 +30,48 @@ namespace TaskLog.WebClient.Data
         //    }).ToArray());
         //}
 
-        public ProjectTask[] GetProjectTasks(DateTime taskTime)
+        public static IEnumerable<TaskClass> TaskClasses =>
+            new List<TaskClass>() {
+                new TaskClass(){Id = 1, Description = "Project 1 SW", Code = "32547-A-210-400", DefaultColor = "LIGHTCORAL"},
+                new TaskClass(){Id = 2, Description = "Project 300 HW", Code = "78548-A-210-400", DefaultColor = "MOCCASIN"},
+                new TaskClass(){Id = 3, Description = "Project 16513 SW", Code = "65894-A-210-400", DefaultColor = "LIGHTCYAN"},
+                new TaskClass(){Id = 4, Description = "Project 54217 SW", Code = "54127-A-210-400", DefaultColor = "GAINSBORO"},
+
+            };
+
+        //public ProjectTask[] GetProjectTasks(DateTime taskTime)
+        //{
+        //    return Enumerable.Range(1, 5).Select(index => GetProjectTask(taskTime)).ToArray();
+        //}
+
+        public TaskInstance[] GetTaskInstances(DateTime taskTime)
         {
-            return Enumerable.Range(1, 5).Select(index => GetProjectTask(taskTime)).ToArray();
+            return Enumerable.Range(1, 5).Select(index => GetTaskInstance(taskTime)).ToArray();
         }
 
         private readonly Random _rng = new Random();
 
-        public ProjectTask GetProjectTask(DateTime taskTime)
+        public TaskInstance GetTaskInstance(DateTime taskTime)
         {
-            return new ProjectTask()
+            return new TaskInstance()
             {
                 Id = id++,
-                Name = $"Task {_rng.Next(0, 55)}",
+                Date = taskTime.Date,
                 Hours = (double)(_rng.Next(0, 6)) / 2,
-                DateTime = taskTime,
+                TaskClass = TaskClasses.ElementAt(_rng.Next()%4),
+                TaskType = (TaskType)(_rng.Next()%5),
             };
         }
+
+        //public ProjectTask GetProjectTask(DateTime taskTime)
+        //{
+        //    return new ProjectTask()
+        //    {
+        //        Id = id++,
+        //        Name = $"Task {_rng.Next(0, 55)}",
+        //        Hours = (double)(_rng.Next(0, 6)) / 2,
+        //        DateTime = taskTime,
+        //    };
+        //}
     }
 }
