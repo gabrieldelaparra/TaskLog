331e8885f6fa7937397c84350b9993095d9e76f9
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2020-09-07 10:23:40 +0200

    add projectType. Closes #17

--- TaskLog.WebClient/Models/ProjectJob.cs
+++ TaskLog.WebClient/Models/ProjectJob.cs
@@ -8,6 +8,12 @@ namespace TaskLog.WebClient.Models
         public string Code { get; set; }
         public string Description { get; set; }
         public string DefaultColor { get; set; }
+        public ProjectType ProjectType {get;set;}
         public override string ToString() => $"({Code}) {Description}";
+    }
+    public enum ProjectType {
+        ZCAT01 = 0,
+        EP0420 = 1,
+        EP0430 = 2
     }
 }
