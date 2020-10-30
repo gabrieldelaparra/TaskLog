0edb8fd6cc22cc8efdbecc4ac462413decf5ec3c
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2020-10-30 08:00:58 +0100

    WIP: Implement MVVM pattern.

---
+++ TaskLog.Core/Models/TaskInstance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TaskLog.Core.Models
+{
+    public class TaskInstance
+    {
+        public Guid Id { get; set; }
+        public Guid ProjectId { get; set; }
+        public Guid ProjectTaskId { get; set; }
+        public DateTime Date { get; set; }
+        public double Hours { get; set; }
+        public string Details { get; set; }
+        public TaskInstanceType TaskInstanceType { get; set; } = TaskInstanceType.Normal;
+    }
+}
---
+++ TaskLog.Core/ViewModels/DayViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace TaskLog.Core.ViewModels
+{
+    public class DayViewModel
+    {
+        private ObservableCollection<TaskInstanceViewModel> _taskInstances =
+                new ObservableCollection<TaskInstanceViewModel>();
+
+        public ObservableCollection<TaskInstanceViewModel> TaskInstances
+        {
+            get { return _taskInstances; }
+            set { _taskInstances = value; }
+        }
+
+        public double DayHours => TaskInstances.Sum(x => x.Hours);
+
+    }
+}
