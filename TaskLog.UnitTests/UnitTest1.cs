d97d04d3e0def95cbee803caca66409bfd4fef53
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2020-10-30 08:01:36 +0100

    Add testing. WIP

---
+++ TaskLog.UnitTests/UnitTest1.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+using TaskLog.Core;
+using TaskLog.Core.Models;
+using TaskLog.Core.ViewModels;
+using Xunit;
+
+namespace TaskLog.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestActionNotifies()
+        {
+            var vm = new TaskInstanceViewModel();
+            vm.NotifyDateChanged = NotifyDateChanged;
+            vm.Date = DateTime.Now.Date;
+        }
+
+        private void NotifyDateChanged(TaskInstanceViewModel obj) {
+            Console.WriteLine($"Date changed: {obj.Date}");
+        }
+
+        [Fact]
+        public void TestCycleEnumFirstToSecond()
+        {
+
+            var length = Enum.GetValues(typeof(TaskInstanceType)).Length;
+            if (length < 2) 
+                Assert.False(true);
+
+            const TaskInstanceType typeFirst = (TaskInstanceType)0;
+            const TaskInstanceType typeSecond = (TaskInstanceType)1;
+            var actual = typeFirst.Cycle();
+            Assert.Equal(typeSecond, actual);
+        }
+
+        [Fact]
+        public void TestCycleEnumLastToFirst()
+        {
+            var length = Enum.GetValues(typeof(TaskInstanceType)).Length;
+            if (length < 2)
+                Assert.False(true);
+
+            var typeLast = (TaskInstanceType)length;
+            const TaskInstanceType typeFirst = (TaskInstanceType)0;
+            var actual = typeLast.Cycle();
+            Assert.Equal(typeFirst, actual);
+        }
+    }
+}
