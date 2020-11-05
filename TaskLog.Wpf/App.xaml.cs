edef334bb2ef93ef23e2da4fdfaf6f9bf25a191d
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2020-11-05 20:47:31 +0100

    Add the Wpf Project; WIP

---
+++ TaskLog.Wpf/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MvvmCross;
+using MvvmCross.Core;
+using MvvmCross.IoC;
+using MvvmCross.Platforms.Wpf.Core;
+using MvvmCross.Platforms.Wpf.Views;
+using TaskLog.Core.Services;
+
+namespace TaskLog.Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : MvxApplication
+    {
+        protected override void RegisterSetup()
+        {
+            this.RegisterSetupType<MvxWpfSetup<Core.App>>();
+        }
+
+        
+    }
+}
---
+++ TaskLog.Wpf/Views/HomeView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MvvmCross.Platforms.Wpf.Views;
+
+namespace TaskLog.Wpf.Views
+{
+    /// <summary>
+    /// Interaction logic for HomeView.xaml
+    /// </summary>
+    public partial class HomeView : MvxWpfView
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
